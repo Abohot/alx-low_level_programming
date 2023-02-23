e1f4374f5d551a3d89f2923326e7ac726673b48c
Abohot <terveraboho@gmail.com>
2023-02-23 12:23:20 -0800

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 {
 int number = 48;
-for (number = 48; number < 57; number++)
+for (number = 48; number < 58; number++)
 {
 _putchar(number);
 }
-_putchar(9);
+_putchar(10);
 }
